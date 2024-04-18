9a6c9a5979ae695a66b3370fb17c5bb1ae143d05
Karolina Anderson <karolina193.10@gmail.com>
2024-04-18 16:03:33 +0200

    added const init

--- js/script.js
+++ js/script.js
@@ -1,40 +1,32 @@
 {
-  const welcome = () => {
-    console.log("Welcome on my page! ;)");
-  };
+  const onFormSubmit = (event) => {
+    event.preventDefault();
 
-  const form = () => {
-    const formElement = document.querySelector(".js-form");
     const amountElement = document.querySelector(".js-amount");
     const currencyElement = document.querySelector(".js-currency");
     const resultElement = document.querySelector(".js-result");
 
-    formElement.addEventListener("submit", (event) => {
-      event.preventDefault();
+    const amount = amountElement.value;
+    const eur = amount * 0.2604;
+    const usd = amount * 0.2352;
+    const cad = amount * 0.1782;
 
-      console.log("Formularz został wysłany");
-
-      const amount = amountElement.value;
-      const eur = amount * 0.2604;
-      const usd = amount * 0.2352;
-      const cad = amount * 0.1782;
-
-      switch (currencyElement.value) {
-        case "EUR":
-          resultElement.innerText = `Wynik: ${eur.toFixed(2)} €`;
-          break;
-        case "USD":
-          resultElement.innerText = `Wynik: ${usd.toFixed(2)} $`;
-          break;
-        case "CAD":
-          resultElement.innerText = `Wynik: ${cad.toFixed(2)} C$`;
-          break;
-      }
-
-      console.log(amountElement.value);
-    });
+    switch (currencyElement.value) {
+      case "EUR":
+        resultElement.innerText = `Wynik: ${eur.toFixed(2)} €`;
+        break;
+      case "USD":
+        resultElement.innerText = `Wynik: ${usd.toFixed(2)} $`;
+        break;
+      case "CAD":
+        resultElement.innerText = `Wynik: ${cad.toFixed(2)} C$`;
+        break;
+    }
   };
 
-  welcome();
-  form();
+  const init = () => {
+    const formElement = document.querySelector(".js-form");
+    formElement.addEventListener("submit", onFormSubmit);
+  };
+  init();
 }
